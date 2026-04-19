9ba7a0dcb82aa1be73f29fa14cf4ef8b6ba8b3f2
cninet <cninet.std@gmail.com>
2026-04-19 12:26:08 +0700

    Remove Unnecessary Delete Announcement Page

--- src/app/announcement/delete/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
-import { Suspense } from "react";
-import Loading from "@/components/Loading";
-import { redirect } from "next/navigation";
-import EditAnnouncementPanel from "@/components/EditAnnouncementPanel";
-import getAnnouncement from "@/libs/getAnnouncement";
